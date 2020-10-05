e51dddb0f4a02803b3a7200889f27445661e05fa
Kraljiccar <nemanjakraljic@gmail.com>
2020-10-05 21:41:17 +0000

    heroku problem

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-if 'DATABASES_URL' in os.environ:
+if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
